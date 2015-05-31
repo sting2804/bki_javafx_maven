cc47fb9921500f40ae8bdd69e043328644a45bb7
sting2804 <slinnik284@gmail.com>
2015-06-01 00:32:34 +0300

    the graph builds right

--- DesktopApp/src/main/java/com/privat/bki/desktopapp/gui/statistic/StatisticWindowController.java
+++ DesktopApp/src/main/java/com/privat/bki/desktopapp/gui/statistic/StatisticWindowController.java
@@ -4,29 +4,31 @@ import com.privat.bki.desktopapp.gui.main.MainModel;
 import com.privat.bki.desktopapp.utils.ChartDrawer;
 import javafx.event.ActionEvent;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
 
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by sting on 5/31/15.
- */
 public class StatisticWindowController {
 
     public ComboBox<String> bankNameComboBox;
-    public AreaChart<Number,Number> areaChart;
+    public BarChart<Number,Number> areaChart;
     public Button applyButton;
+    public Pane panel;
     private MainModel mainModel;
     private Map<String, String> bankMap;
 
     public void drawChart(ActionEvent actionEvent) {
         String bankName = bankNameComboBox.getValue();
         Map<String,List> stat = mainModel.getStatisticForBank(bankName);
-        if(bankName!=null)
-            areaChart= (AreaChart<Number, Number>) ChartDrawer.drawStatisticForBankByYears(stat,bankName, areaChart);
-
+        if(bankName!=null) {
+            AreaChart<Number, Number> areaChart = ChartDrawer.drawStatisticForBankByYears(stat, bankName);
+            panel.getChildren().clear();
+            panel.getChildren().addAll(areaChart);
+        }
     }
 
     public void setMainModel(MainModel mainModel) {
--- DesktopApp/src/main/java/com/privat/bki/desktopapp/utils/ChartDrawer.java
+++ DesktopApp/src/main/java/com/privat/bki/desktopapp/utils/ChartDrawer.java
@@ -1,34 +1,28 @@
 package com.privat.bki.desktopapp.utils;
 
 import javafx.scene.Scene;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.Chart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by sting on 5/31/15.
- */
 public class ChartDrawer {
 
-    public static Chart drawStatisticForBankByYears(Map<String,List> statistic, String bankName, AreaChart<Number,Number> areaChart){
+    public static AreaChart<Number,Number> drawStatisticForBankByYears(Map<String,List> statistic, String bankName){
         List<Map<String,Integer>> bankInfo = statistic.get(bankName);
         List<Integer> years = new ArrayList<>();
         List<Integer> count = new ArrayList<>();
-        for(int i=0; i < bankInfo.size(); i++){
-            years.add(bankInfo.get(i).get("year"));
-            count.add(bankInfo.get(i).get("count"));
+        for (Map<String, Integer> aBankInfo : bankInfo) {
+            years.add(aBankInfo.get("year"));
+            count.add(aBankInfo.get("count"));
         }
         final NumberAxis xAxis = new NumberAxis(Collections.min(years),Collections.max(years),1);
-        final NumberAxis yAxis = new NumberAxis(0,Collections.max(count),0.1);
-        //areaChart = new AreaChart<Number, Number>(xAxis,yAxis);
+        final NumberAxis yAxis = new NumberAxis(0,Collections.max(count),1);
+        AreaChart<Number,Number> areaChart = new AreaChart<>(xAxis, yAxis);
         areaChart.setTitle("Статистика для "+bankName);
-
+        areaChart.setMaxSize(520,320);
         XYChart.Series series= new XYChart.Series();
         series.setName(bankName);
         for(int i=0; i<years.size(); i++){
